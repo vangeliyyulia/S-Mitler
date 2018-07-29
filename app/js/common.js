224db6dd823a7140e710e6a24476c04dc0fab8bd
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-07-29 22:32:30 +0300

    Header Content

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,18 @@
 $(function() {
-    $('#my-menu').mmenu()
+    $('#my-menu').mmenu({
+		extensions: [ 'widescreen', 'theme-black', 'effect-menu-slide', 'pagedim-black' ],
+		navbar: {
+			title: '<img src="img/logo-1.svg" alt="Салон красоты Смитлер">'
+		},
+		offCanvas: {
+			position  : 'right'
+		}
+	});
+
+	var api = $('#my-menu').data('mmenu');
+	api.bind('opened', function () {
+		$('.hamburger').addClass('is-active');
+	}).bind('closed', function () {
+		$('.hamburger').removeClass('is-active');
+	});
 });
