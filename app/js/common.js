54c269ced7dcd52532ee2815d136e984a4690eea
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-07-31 23:08:51 +0300

    Slider

--- app/js/common.js
+++ app/js/common.js
@@ -15,4 +15,33 @@ $(function() {
 	}).bind('closed', function () {
 		$('.hamburger').removeClass('is-active');
 	});
+
+	$('.services__slider').owlCarousel({
+		loop: true,
+		nav: true,
+		smartSpeed: 700,
+		navText: ['<i class="fa fa-angle-double-left"></i>','<i class="fa fa-angle-double-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1,
+			},
+			800: {
+				items: 2,
+			},
+			1100: {
+				items: 3,
+			}
+		}
+	}).on('changed.owl.carousel', function() {
+		carouselService();
+	});
+
+	function carouselService() {
+		$('.services__slider-item').each(function() {
+			var ths = $(this);
+			var thsh = ths.find('.services__slider-content').outerHeight();
+			ths.find('.services__services-image').css('min-height', thsh);
+		});
+	}carouselService();
 });
