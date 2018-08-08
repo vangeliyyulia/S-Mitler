5dca195d21667e659becfb22cb225099671331f9
Julia Vangeliy <vangeliy_yulia@mail.ru>
2018-08-08 15:55:33 +0300

    Footer Pt.2

--- app/js/common.js
+++ app/js/common.js
@@ -98,4 +98,15 @@ $(function() {
 
 	$(".partners__item").equalHeights();
 
+	$('.top').click(function() {
+		$('html, body').stop().animate({scrollTop: 0}, 'slow', 'swing');
+	});
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > $(window).height()) {
+			$('.top').addClass("active");
+		} else {
+			$('.top').removeClass("active");
+		};
+	});
+
 });
